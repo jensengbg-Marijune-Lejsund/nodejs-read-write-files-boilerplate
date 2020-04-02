84a4c0bc271303bbb0a6d27a18fe203139dc5712
jensengbg-Marijune-Lejsund <59868177+jensengbg-Marijune-Lejsund@users.noreply.github.com>
2020-04-02 13:21:05 +0200

    oscar

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
 const fs = require('fs');
-fs.readFile('quote.txt', 'utf8', (error, contents) => {
-    console.log(contents);
+
+
+fs.readFile('OscarWilde.txt', 'utf8', (error, contents) => {
+    console.log('Innehållet i OscarWilde.txt är: ', contents);
+});
+
+fs.writeFile('OscarWilde.txt', quote, (error) => {
+    console.log('Quote saved');
 });
